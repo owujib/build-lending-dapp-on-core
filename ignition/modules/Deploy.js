42320dd73f2b09891a44c3fe804618dca9d0ada6
owujib <owujibfavour@gmail.com>
2024-08-14 01:10:02 +0100

    code update

--- ignition/modules/Deploy.js
+++ ignition/modules/Deploy.js
@@ -1,1 +1,7 @@
 // Add your deployment script
+const { buildModule } = require('@nomicfoundation/hardhat-ignition/modules');
+
+module.exports = buildModule('CoreLoanPlatform', (m) => {
+  const contract = m.contract('CoreLoanPlatform', []);
+  return { contract };
+});
